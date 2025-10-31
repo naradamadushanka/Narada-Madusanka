a60845096621201e216c782828c82b14d0443ceb
naradamadushanka <naradamadushanka293@gmail.com>
2025-10-31 17:32:49 +0530

    prime

--- test.py
+++ test.py
@@ -1,21 +1,10 @@
-def seq(n):
-    l=[]
-    while True:
-        l.append(n)
-        if n==1:
-            break
-        if n%2==0:
-            n=n/2
-            
-        else:
-            n=(3*n)+1
-            
-    return l
-max=0
-for i in range(1,1000000):
-    if len(seq(i))>max:
-        
-        max=i
-    
-print(max)
-print('hi')
+def gen(n):
+    for i in range(n):
+        yield i * i
+x=gen(5)
+for i in x:
+    print(i)
+print(x)
+print('-------------------')
+for i in gen(10):
+    print(i)
